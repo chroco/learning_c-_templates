e34f3631168a6cbcf855da9077a395d51e3efa01
Chad Coates <chroco@gmail.com>
2018-09-24 23:26:48 -0700

    saved it

---
+++ app_template/src/main.cpp
@@ -0,0 +1,20 @@
+
+/**
+ *  @file main.cpp
+ *
+ *  have fun storming the castle! 
+ *
+ *  o_0
+ */
+
+#include "main.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  cout << "o_0\n";
+
+  return 0;
+}
---
+++ app_template/src/main.hpp
@@ -0,0 +1,14 @@
+
+/**
+ *  @file main.hpp
+ *
+ *  o_0
+ */
+
+
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
+
+#endif
+
