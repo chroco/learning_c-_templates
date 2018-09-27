dfcf8274799069364ac18c0f30f10cdbe67ecd1b
Chad Coates <chroco@gmail.com>
2018-09-26 17:54:25 -0700

    testing trailing_decl wtfgit add *!

--- misc/trailing_decl/src/main.cpp
+++ misc/trailing_decl/src/main.cpp
@@ -13,7 +13,13 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-  cout << "o_0\n";
+  cout << "o_0\n\n";
+
+  auto a = 2;
+  auto b = 2.3;
+  cout << decay_max(a,b) << '\n';
+  cout << auto_max(a,b) << '\n';
+  cout << decay_max(a,b) << '\n';
 
   return 0;
 }
